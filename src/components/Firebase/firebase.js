767f1f179533a0958f601ef8c5de22c4b6a30265
Rob Salzberg <robsalzberg@gmail.com>
2019-02-21 23:09:48 -0500

    Created signup page with Firebase Authentication

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -23,6 +23,14 @@ const config = {
 
     doSignInWithEmailAndPassword = (email, password) =>
         this.auth.signInWithEmailAndPassword(email, password);
-  }
+    
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
+
+}
   
   export default Firebase;
--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,7 +1,7 @@
-import FirebaseContext from './context';
+import FirebaseContext, { withFirebase } from './context';
 import Firebase from './firebase';
 
 export default Firebase;
 
-export { FirebaseContext };
+export { FirebaseContext, withFirebase  };
 
